8827a50fa27bb1f03694593ee126ebebb05ed8cc
Ariana Giorgi <arianangiorgi@gmail.com>
2014-04-01 19:25:19 -0400

    works for books.
    needs to work for organization and books AND organizations

--- part2.java
+++ part2.java
@@ -10,25 +10,71 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
+import java.util.*;
+import java.util.Map.Entry;
+import org.apache.commons.lang3.StringUtils;
+
 public class part2 {
-  public static void main(String[] args) {
-    try {
-      HttpTransport httpTransport = new NetHttpTransport();
-      HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
-      JSONParser parser = new JSONParser();
-      String query = "[{\"/visual_art/visual_artist/artworks\":[{\"a:name\":null,\"name~=\":\"Mona Lisa\"}],\"id\":null,\"name\":null,\"type\":\"/visual_art/visual_artist\"}]";
-      GenericUrl url = new GenericUrl("https://www.googleapis.com/freebase/v1/mqlread");
-      url.put("query", query);
-      url.put("key", "AIzaSyCIQ8gDGEMgxJpSsGK6BwkfLZXtN4MTf4E");
-      HttpRequest request = requestFactory.buildGetRequest(url);
-      HttpResponse httpResponse = request.execute();
-      JSONObject response = (JSONObject)parser.parse(httpResponse.parseAsString());
-      JSONArray results = (JSONArray)response.get("result");
-      for (Object result : results) {
-        System.out.println(JsonPath.read(result,"$.name").toString());
-      }
-    } catch (Exception ex) {
-      ex.printStackTrace();
-    }
-  }
+	public static void main(String[] args) {
+		try {
+
+			String accountKey = "AIzaSyCIQ8gDGEMgxJpSsGK6BwkfLZXtN4MTf4E";
+
+			HttpTransport httpTransport = new NetHttpTransport();
+			HttpRequestFactory requestFactory = httpTransport.createRequestFactory();
+			JSONParser parser = new JSONParser();
+
+			//need to make query for book AND organization
+			String query = "[{\"/book/author/works_written\":[{\"a:name\":null,\"name~=\":\"Lord of the Rings\"}],\"id\":null,\"name\":null,\"type\":\"/book/author\"}]";
+
+			GenericUrl url = new GenericUrl("https://www.googleapis.com/freebase/v1/mqlread");
+			url.put("query", query);
+			url.put("key", accountKey);
+			HttpRequest request = requestFactory.buildGetRequest(url);
+			HttpResponse httpResponse = request.execute();
+			JSONObject response = (JSONObject)parser.parse(httpResponse.parseAsString());
+			JSONArray results = (JSONArray)response.get("result");
+			
+			Map<String, String> resultList = new TreeMap<String, String>(); 
+			for (Object result : results) {
+				String name = JsonPath.read(result,"$.name").toString();
+				ArrayList<String> creations = new ArrayList<String>();
+				String type = null;
+				//for an author
+				if(JsonPath.read(result,"$.type").toString().contains("/book/author")){
+					type = "Author";
+					int count = StringUtils.countMatches(JsonPath.read(result,"$./book/author/works_written").toString(), "a:name");
+					for(int i=0; i<count; i++){
+						creations.add(JsonPath.read(result,"$./book/author/works_written.a:name["+i+"]").toString());
+					}
+				}else{
+					type = "?";
+				}
+				//for a businessperson
+
+				//compressing their creations
+				String creationStr = "";
+				for(int i=0; i<creations.size(); i++){
+					if(i==0){
+						creationStr = creationStr +"<"+ creations.get(i) + ">";
+					}else if(creations.size()>2 && i==creations.size()-1){
+						creationStr = creationStr + ", and <" + creations.get(i) +">";
+					}else if(creations.size()==2 && i==creations.size()-1){
+						creationStr = creationStr + " and <" + creations.get(i) +">";
+					}else{
+						creationStr = creationStr + ", <" + creations.get(i) + ">";
+					}
+				}
+				resultList.put(name, name+" (as "+type+") created "+creationStr);
+			}
+			int i = 0;
+			for(Entry<String, String> entry : resultList.entrySet()){
+				i = i +1;
+				System.out.println(i+". "+entry.getValue());
+			}
+		
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 }
